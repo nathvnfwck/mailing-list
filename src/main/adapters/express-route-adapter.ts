2c035102fd15c062f07485fc3249206279a56824
nathvnfwck <nathaanscout@icloud.com>
2023-03-26 15:33:26 -0300

    feat: support express route adapter to file

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,3 +1,5 @@
+import { PassThrough } from 'node:stream'
+
 import { type Request, type Response } from 'express'
 
 import { type Controller } from '@/adapters/presentation/controllers/ports/controller'
@@ -6,13 +8,26 @@ import { type HttpRequest } from '@/adapters/presentation/controllers/ports/http
 export const adaptRoute = (controller: Controller) => {
   return async (request: Request, response: Response) => {
     const httpRequest: HttpRequest = {
-      body: request.body
+      body: {
+        ...request.body,
+        ...request.query
+      }
     }
 
     const httpResponse = await controller.handle(httpRequest)
 
     response.status(httpResponse.statusCode)
 
+    if (httpResponse.file) {
+      response.set('Content-disposition', 'attachment; filename=users.csv')
+      response.set('Content-type', 'text/plain')
+
+      const readStream = new PassThrough()
+      readStream.end(httpResponse.file)
+
+      return readStream
+        .pipe(response)
+    }
     if (httpRequest.body) response.json(httpResponse.body)
   }
 }
